e9e713df906b706ba59d6f06c0602e2387484a28
Johan Viancha <vianchajohan@gmail.com>
2025-06-13 16:16:05 -0500

    dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,93 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 const deps = require("./package.json").dependencies;
 
-const printCompilationMessage = require('./compilation.config.js');
+const printCompilationMessage = require("./compilation.config.js");
 
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: "https://colorlist.netlify.app/",
-  },
+module.exports = (_, argv) => (_, argv) => {
+  return {
+    output: {
+      publicPath:
+        argv.mode === "development"
+          ? "http://localhost:3002/"
+          : "https://colorpicker-microfrontend.netlify.app/",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 3002,
-    historyApiFallback: true,
-    watchFiles: [path.resolve(__dirname, 'src')],
-    onListening: function (devServer) {
-      const port = devServer.server.address().port
+    devServer: {
+      port: 3002,
+      historyApiFallback: true,
+      watchFiles: [path.resolve(__dirname, "src")],
+      onListening: function (devServer) {
+        const port = devServer.server.address().port;
 
-      printCompilationMessage('compiling', port)
+        printCompilationMessage("compiling", port);
 
-      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-        setImmediate(() => {
-          if (stats.hasErrors()) {
-            printCompilationMessage('failure', port)
-          } else {
-            printCompilationMessage('success', port)
-          }
-        })
-      })
-    }
-  },
+        devServer.compiler.hooks.done.tap("OutputMessagePlugin", (stats) => {
+          setImmediate(() => {
+            if (stats.hasErrors()) {
+              printCompilationMessage("failure", port);
+            } else {
+              printCompilationMessage("success", port);
+            }
+          });
+        });
+      },
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "mf_colorlist",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./ColorList": "./src/components/ColorList.jsx",
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv(),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "mf_colorlist",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./ColorList": "./src/components/ColorList.jsx"
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
-});
+  };
+};
